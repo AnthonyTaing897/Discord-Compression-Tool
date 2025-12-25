dddf76f6ad6dec2f68d78f1e2adb4a992a0e3edc
Loafcifer <taing897@gmail.com>
2025-12-25 16:38:44 +1100

    Updated webhook receiver
    To fit the new webhook format, the receiver was updated to allow it to process the new formatted posts

--- Discord_Bot/server/webhookReceiver.py
+++ Discord_Bot/server/webhookReceiver.py
@@ -1,6 +1,8 @@
 import discord
+import requests
 from flask import Flask, request, abort
 from waitress import serve
+from server.func.Compress_Video_Function import compressVid
 import threading
 
 app = Flask(__name__)
@@ -15,8 +17,39 @@ def webhook():
     if request.method == 'POST':
         data = request.get_json() or {}
         print(data)
-        urls = data.get('data')
+        urls = data.get('data').get('url', [])
         print(urls)
+        if urls:
+            for url in urls:
+                try:
+                    x = url.get('url')
+                    response = requests.get(x)
+                    with open("Temp Videos/testfile.mp4", 'wb') as f:
+                        f.write(response.content)
+                        f.close()
+                except Exception as e:
+                    print(f"Error downloading file from {url}: {e}")
+
+        if _bot: 
+                user_id = data.get('data').get('UserID')
+
+                processedVid = compressVid(video_file="Temp Videos/testfile.mp4",
+                                processed_drct="Discord_Bot/cmds/Library/Processed_Videos",
+                                filename="testfile.mp4",
+                                target_file_size=8,
+                                compressAudio=True)
+                
+                async def send_message(file_path, user_id):
+                    try:
+                        user = await _bot.fetch_user(int(user_id))
+                        await user.send("Here is your compressed video:")  
+                        await user.send(file=discord.File(file_path))
+                    except Exception as e:
+                        print(f"Error sending message to user {user_id}: {e}")
+            
+                _bot.loop.create_task(send_message(processedVid, user_id))
+
+                
         '''
         user_id = data.get('payload').get('data').get('UserID')
         print(user_id)
@@ -36,7 +69,7 @@ def webhook():
             _bot.loop.create_task(send_message())
 
         '''
-        return 'success', 200
+        return {"status": "success"}
     
     else:
         abort(400)
